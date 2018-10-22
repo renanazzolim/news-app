0a6eea1031fc32d6ef89551ac93383220f175946
Renan Azzolim <azzolimrenan2@gmail.com>
2018-10-22 18:52:43 -0300

    express validatior

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,15 +1,29 @@
 module.exports = function (application) {
     application.get('/formulario_inclusao_noticia', function (req, res) {
-        res.render('admin/form_add_noticia');
+        res.render('admin/form_add_noticia', {validacao: {}, noticia: {}});
     });
 
     application.post('/noticias/salvar', function (req, res) {
         // body parser -> npm install body-parser --save
-        // var noticia = req.body; 
+        var noticia = req.body; 
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor', 'Autor é obrigatório').notEmpty();
+        req.assert('data_noticia', 'Data é obrigatória').notEmpty();
+        req.assert('noticia', 'Notícia é obrigatória').notEmpty();
+
+        var errors = req.validationErrors();
+
+        if (errors) {
+            res.render('admin/form_add_noticia', {validacao: errors, noticia: noticia});
+            return;
+        }
+
         var connection = application.config.dbConnection();
         var noticiasModel = new application.app.models.NoticiasDAO(connection);
 
-        noticiasModel.salvarNoticia(req.body, function (error, result) {
+        noticiasModel.salvarNoticia(noticia, function (error, result) {
             res.redirect('/noticias');
         });
     });
--- config/server.js
+++ config/server.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var consign = require('consign');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 var app = express();
 app.set('view engine', 'ejs'); // altera tipo de engine para poder separar o html do código principal
 app.set('views', './app/views'); // caminho de views (alterado o padrão do app.js (main) para o diretório atual)
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 /*
 automaticamente vê todos os requires do diretório setado e disponibiliza dentro 
